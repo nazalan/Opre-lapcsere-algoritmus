c9181dc0b064d8cba975cdd547db2939b37dc3b2
nazalan <zalan2552@gmail.com>
2023-04-26 19:59:02 +0200

    kesz

--- Csere.java
+++ Csere.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Csere {
 
     public String str="";
@@ -7,69 +10,79 @@ public class Csere {
 
     public int idx=0;
     void fun(int[] data, int db){
+
         //init
-        int[][] res=new int[3][4];
-        for(int i=0; i<3; i++){
-            for (int j=0; j<4; j++){
-                res[i][j]=0;
-            }
-        }
+        LinkedList<Lap> lapok =new LinkedList<>();
+        lapok.add(new Lap(0, 0, 0, 0, 0));
+        lapok.add(new Lap(1, 0, 0, 0, 0));
+        lapok.add(new Lap(2, 0, 0, 0, 0));
 
         for(int d=0; d<db; d++){
             for(int i=0; i<3; i++){
-                //System.out.println("data: "+data[d] +"num: "+ i+ " mi van benne: " + res[i][1]+ " miota: "+ res[i][2]+ " hivatkozott: "+ res[i][3]);
+                //System.out.println("data: "+data[d] +"num: "+ lapok.get(i).id+ " mi van benne: " + lapok.get(i).mivanbenne+ " miota: "+ lapok.get(i).miota+ " hivatkozott: "+ lapok.get(i).hivtakozott+ " fagyasztott: "+lapok.get(i).fagyasztott);
             }
             //System.out.println("data: "+data[d]);
-            for(int i=0; i<3; i++){//benne van
-                if(res[i][1]==data[d]){
-                    str+='-';
-                    bennevan=true;
-                    res[i][3]=1;
-                    break;
-                }
-            }
 
-            boolean fut=true;
-            if(!bennevan){
-                while(fut){
-                    for(int i=(idx%3); i<3; i++){//szabad helyre rakás
-                        System.out.println("data: "+data[d] +"num: "+ i+ " mi van benne: " + res[i][1]+ " miota: "+ res[i][2]+ " hivatkozott: "+ res[i][3]);
-                        idx=0;
-                        if(res[0][2]<4 && res[1][2]<4 && res[2][2]<4 && res[0][2]>0 && res[1][2]>0 && res[2][2]>0) {
-                            str+='*';
-                            counter++;
-                            fut=false;
-                            idx=i;
-                            break;
-                        }
-
-                        if((res[i][0]==0 || res[i][2]>3) && res[i][3]==0){//belerakás
-                            str+=(char)('A'+i);
-                            counter++;
-                            res[i][0]=1;
-                            res[i][2]=0;
-                            //res[i][3]=0;
-                            res[i][1]=data[d];
-                            fut=false;
-                            idx=i;
-                            break;
-                        }
-                        //System.out.println(i);
-                        res[i][3]=0;
+            if(!lapok.isEmpty()){
+                for(int i=0; i<3; i++){//benne van
+                    if(lapok.get(i).mivanbenne==data[d]){
+                        str+='-';
+                        bennevan=true;
+                        lapok.get(i).hivtakozott=1;
+                        break;
                     }
                 }
-                fut=true;
             }
 
+            if(!bennevan){
+                    while(true) {
+                        //nincs hely
+                        if (lapok.get(0).fagyasztott ==1 && lapok.get(1).fagyasztott ==1 && lapok.get(2).fagyasztott ==1) {
+                            str += '*';
+                            counter++;
+                            break;
+                        }
+                        if (lapok.get(idx).fagyasztott == 1) {
+                            //lapok.add(lapok.remove(idx));
+                            idx++;
+                        }
+                        if (lapok.get(idx).hivtakozott == 1) {
+                            lapok.get(idx).hivtakozott = 0;
+                            lapok.add(lapok.remove(idx));
+                        }
+
+                        if ((lapok.get(idx).mivanbenne == 0 || lapok.get(idx).fagyasztott == 0)) {//belerakás
+
+                            if (lapok.get(idx).hivtakozott == 0) {
+                                str += (char) ('A' + lapok.get(idx).id);
+                                counter++;
+                                lapok.get(idx).miota = 0;
+                                lapok.get(idx).hivtakozott = 0;
+                                lapok.get(idx).mivanbenne = data[d];
+                                lapok.get(idx).fagyasztott = 1;
+                                lapok.get(idx).miota = 4;
+                                lapok.add(lapok.remove(idx));
+                                break;
+                            }
+                        }
+                    }
+                idx=0;
+
+            }
+
+
             for(int i=0; i<3; i++){//mióta növelése
-                if(res[i][1]!=0){
-                    res[i][2]++;
+                if(lapok.get(i).fagyasztott==1){
+                    lapok.get(i).miota--;
+                    if( lapok.get(i).miota==0){
+                        lapok.get(i).fagyasztott=0;
+                    }
                     //System.out.println("melyik: "+i + "miota: "+res[i][2]);
                 }
             }
 
             bennevan=false;
-            System.out.println();
+            //System.out.println();
         }
 
         System.out.println(str);
---
+++ Lap.java
@@ -0,0 +1,16 @@
+public class Lap {
+    int id;
+    int mivanbenne;
+    int miota;
+    int hivtakozott;
+
+    int fagyasztott;
+
+    public Lap(int i, int m, int mo, int h, int f){
+        id=i;
+        mivanbenne=m;
+        miota=mo;
+        hivtakozott=h;
+        fagyasztott=f;
+    }
+}
